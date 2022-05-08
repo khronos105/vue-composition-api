d2014719a647e8dd2c1a4ef89d4c9fb74765438b
Roma Bilibov <romabilibov@gmail.com>
2022-05-08 14:28:54 +0200

    Finished composition api

---
+++ src/views/SearchPokemon.vue
@@ -0,0 +1,42 @@
+<template>
+  <h1>Buscar Pokemon: {{ pokemonId }}</h1>
+  <form @submit.prevent="onSubmit">
+        <input 
+            type="number"
+            placeholder="Numero de pokemon"
+            v-model="pokemonId"
+            ref="txtSearchId"
+        >
+        <br>
+        <span>Presione enter para buscar</span>
+  </form>
+</template>
+
+<script>
+import { ref, onActivated } from 'vue'
+import { useRouter } from 'vue-router'
+
+export default {
+    setup(){
+
+        const router = useRouter()
+
+        const pokemonId = ref(1)
+
+        const txtSearchId = ref('')
+
+        onActivated(() => {
+            txtSearchId.value.select()
+        })
+
+        return {
+            pokemonId,
+            txtSearchId,
+            onSubmit: () => {
+                //console.log(`Pokemon a buscar ${pokemonId.value}`)
+                router.push({name: 'pokemon-id', params:{ id: pokemonId.value}})
+            }
+        }
+    }
+}
+</script>
---
+++ src/views/Users.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <h2 v-if="isLoading">Espere por favor...</h2>
+    <h2 v-else>Usuarios</h2>
+    <h5 v-if="errorMessage">{{ errorMessage }}</h5>
+  </div>
+
+  <div v-if="users.length > 0">
+      <ul>
+          <li v-for="{first_name, last_name, email, id} in users" :key="id">
+              <h4>{{ first_name }} {{ last_name }}</h4>
+              <h6>{{ email }}</h6>
+          </li>
+      </ul>
+  </div>
+
+  <button @click="prevPage">Atras</button>
+  <button @click="nextPage">Siguiente</button>
+  <span>Pagina {{ currentPage }}</span>
+</template>
+
+<script >
+import useUsers from '@/composables/useUsers'
+
+export default {
+    setup(){
+        const {
+            currentPage,
+            errorMessage,
+            isLoading,
+            nextPage,
+            prevPage,
+            users
+        } = useUsers()
+
+        return {
+            currentPage,
+            errorMessage,
+            isLoading,
+            nextPage,
+            prevPage,
+            users
+        }
+    }
+}
+</script>
+
+<style scoped>
+h2{
+    text-align: center;
+}
+div{
+    display: flex;
+    justify-content: center;
+    text-align: center;
+    flex-direction: column;
+}
+ul{
+    width: 250px;
+    margin: 0 auto;
+}
+</style>
